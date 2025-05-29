bee11132b172783933529de691cb9c424d627239
Dilip Chandu Bethu <dileepbethu111@gmail.com>
2025-05-29 14:53:35 +0530

    fix: updated config and deploy changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter base="/redbus-clone"> {/* ✅ Wrap everything */}
+  <BrowserRouter basename="/redbus-clone"> {/* ✅ Wrap everything */}
     <AuthProvider>
       <App />
     </AuthProvider>
