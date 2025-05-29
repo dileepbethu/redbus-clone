5048b751349a299ac100cc79cf5b27d57455bb78
Dilip Chandu Bethu <dileepbethu111@gmail.com>
2025-05-29 10:57:14 +0530

    updating vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
   },
-  base: '/redbus-clone/', // For GitHub Pages
+  base: ' https://dileepbethu.github.io/redbus-clone/', // For GitHub Pages
   define: {
     'import.meta.env.VITE_API_BASE_URL': JSON.stringify('https://redbus-clone-3.onrender.com'),
   },
